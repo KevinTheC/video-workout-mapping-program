c1869c104b9f0e0111a594518f28f36c1f638b50
kevin <kevinculkin210@gmail.com>
2026-01-12 17:32:44 -0500

    Added test for FrameBuffer class

--- cpp-core/src/FrameBuffer.cpp
+++ cpp-core/src/FrameBuffer.cpp
@@ -1,44 +1,40 @@
 #include "FrameBuffer.h"
+#include <iostream>
 bool FrameBuffer::shutdown() {
-    if (buffer == nullptr)
+    if (this->maxFrames == 0)
         return false;
-    delete[] buffer;
-    buffer = nullptr;
-    next = nullptr;
-    maxFrames = 0;
+    this->buffer = std::vector<uint8_t>(0);
+    this->maxFrames = 0;
+    this->nextFrame = 0;
     return true;
 };
-bool FrameBuffer::initialize(FrameFormat format, size_t maxFrames) {
-    if (buffer != nullptr)
+bool FrameBuffer::initialize(const FrameFormat& format, size_t maxFrames) {
+    if (this->maxFrames != 0)
         return false;
     this->format = format;
-    this->buffer = new uint8_t[format.getFrameSize() * maxFrames];
-    this->next = buffer;
+    this->buffer = std::vector<uint8_t>();
+    this->buffer.reserve(format.getFrameSize() * maxFrames);
     this->maxFrames = maxFrames;
+    this->nextFrame = 0;
     return true;
 };
 bool FrameBuffer::submitFrame(uint8_t* bufferBegin, size_t length) {
     //these could potentially be exceptions instead
-    if (buffer == nullptr)
-        return false;
-    size_t used = next - buffer;
-    if (length + used > maxFrames) {
+    if (this->maxFrames == 0) {
         return false;
     }
-    if (length != format.getFrameSize()) {
+    if (this->buffer.capacity() < this->buffer.size() + length) {
         return false;
     }
-    std::copy(bufferBegin, bufferBegin + length, next);
-    next += length;
+    if (length != this->format.getFrameSize()) {
+        return false;
+    }
+    std::copy(bufferBegin, bufferBegin + length, this->buffer.begin() + nextFrame);
+    nextFrame += length;
     return true;
 };
-FrameBuffer::FrameBuffer(){
-    this->format = FrameFormat{0, 0, FrameFormat::Format::RGB};
+FrameBuffer::FrameBuffer() : format(FrameFormat{0, 0, FrameFormat::Format::RGB}){
     this->maxFrames = 0;
-    this->buffer = nullptr;
-    this->next = nullptr;
-};
-FrameBuffer::~FrameBuffer(){
-    if (buffer != nullptr)
-        delete[] buffer;
+    this->buffer = std::vector<uint8_t>(0);
+    this->nextFrame = 0;
 };
--- cpp-core/tests/FrameBufferTests.cpp
+++ cpp-core/tests/FrameBufferTests.cpp
@@ -1,6 +1,90 @@
 #include <cassert>
+#include <functional>
+#include <tuple>
 #include <FrameBuffer.h>
+#include <iostream>
+#define getFormat() FrameFormat{1,1, FrameFormat::Format::RGB}
+#define getBuffer() std::vector<uint8_t>(3)
+void testOrderingOfCalls();
+void testCalls(std::function<bool()>,bool,const char*);
 int main() {
-    FrameBuffer fb;
-    return 0;
-}
+    testOrderingOfCalls();
+};
+void testOrderingOfCalls(){
+    using Test = std::tuple<std::function<bool()>, bool, const char*>;
+
+    std::vector<Test> tests = {
+        //default constructor
+        {[](){return FrameBuffer().initialize(getFormat(), 1);}, true, "Initialization failed after default constructor"},
+        {[](){return FrameBuffer().shutdown();}, false, "Shutdown occured with uninitialized buffer after default constructor"},
+        {[](){return FrameBuffer().submitFrame(getBuffer().data(), 3);}, false, "Frame submission occured with uninitialized buffer after default constructor"},
+        
+        //frame submission
+        {[](){
+            FrameBuffer fb;
+            fb.initialize(getFormat(), 1);
+            return fb.submitFrame(getBuffer().data(), 3);
+        }, true, "Frame submission after init failed"},
+        {[](){
+            FrameBuffer fb;
+            fb.initialize(getFormat(), 1);
+            fb.shutdown();
+            return fb.submitFrame(getBuffer().data(), 3);
+        }, false, "Frame submission after shutdown succeeded"},
+        {[](){
+            FrameBuffer fb;
+            fb.initialize(getFormat(), 2);
+            fb.submitFrame(getBuffer().data(), 3);
+            return fb.submitFrame(getBuffer().data(), 3);
+        }, true, "Consecutive frame submissions failed"},
+
+        //initialization
+        {[](){
+            FrameBuffer fb;
+            fb.initialize(getFormat(), 1);
+            return fb.initialize(getFormat(), 1);
+        }, false, "Double initialization succeeded"},
+        {[](){
+            FrameBuffer fb;
+            fb.initialize(getFormat(), 1);
+            fb.shutdown();
+            return fb.initialize(getFormat(), 1);
+        }, true, "Initialize after shutdown failed"},
+        {[](){
+            FrameBuffer fb;
+            fb.initialize(getFormat(), 1);
+            fb.submitFrame(getBuffer().data(), 3);
+            return fb.initialize(getFormat(), 1);
+        }, false, "Initialize after frame submission succeeded"},
+
+        //shutdown
+        {[](){
+            FrameBuffer fb;
+            fb.initialize(getFormat(), 1);
+            return fb.shutdown();
+        }, true, "Shutdown after init failed"},
+        {[](){
+            FrameBuffer fb;
+            fb.initialize(getFormat(), 1);
+            fb.shutdown();
+            return fb.shutdown();;
+        }, false, "Double shutdown succeeded"},
+        {[](){
+            FrameBuffer fb;
+            fb.initialize(getFormat(), 1);
+            fb.submitFrame(getBuffer().data(), 3);
+            return fb.shutdown();
+        }, true, "Shutdown after frame submission failed"}
+    };
+    for (auto& t : tests){
+        testCalls(std::get<0>(t),std::get<1>(t),std::get<2>(t));
+    };
+};
+void testCalls(std::function<bool()> func1,
+    bool expected,
+    const char* onFail){
+        if (!(func1() == expected)){
+            std::cerr << onFail << std::endl;
+            assert(false);
+        }
+};
